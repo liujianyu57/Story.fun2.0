ec4c14ac3ecfc568b2a81b73ab41c8ef2f69fd55
Jianyu <497139081@qq.com>
2026-06-09 18:08:43 +0800

    优化标题栏

--- load-header.js
+++ load-header.js
@@ -271,9 +271,6 @@
   .story-header-wrapper .nav-links {
     display: none;
   }
-  .story-header-wrapper .lang-btn span {
-    display: none;
-  }
   .story-header-wrapper .mobile-drawer-overlay {
     display: block;
   }
@@ -431,7 +428,6 @@
           <path d="M2 12h20"/>
           <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/>
         </svg>
-        <span>Language</span>
       </button>
       <div class="auth-container" id="authContainer"></div>
     </div>
