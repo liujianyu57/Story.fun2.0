5c9c1a9378f8af6602d8622cc93e2c67a86d2b56
Jianyu <497139081@qq.com>
2026-08-01 23:48:11 +0800

    修复移动端首页

--- load-sidebar.js
+++ load-sidebar.js
@@ -77,7 +77,7 @@
                     '</a>' +
                     '<a href="index.html" class="sb-nav-item" data-page="index.html">' +
                         '<svg viewBox="0 0 24 24" stroke-width="1.8"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>' +
-                        '<span class="sb-label">剧场</span>' +
+                        '<span class="sb-label">短剧</span>' +
                     '</a>' +
                     '<a href="actors.html" class="sb-nav-item" data-page="actors.html">' +
                         '<svg viewBox="0 0 24 24" stroke-width="1.8"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M23 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>' +
